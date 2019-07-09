bb9e8dd43ed728b187c6c18d233d1689fb35cd9c
Harry Ghazni <hghazni@gmail.com>
2019-07-09 15:49:24 +0100

    Converts, restructures folders and move files

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+
+const App: React.FunctionComponent = () => {
+    return (
+        <div>Hello Killer Storybook Config</div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
