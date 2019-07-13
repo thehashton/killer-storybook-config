cac5f416089a94c7c54ff0f69e9ccccb748a8c55
Harry Ghazni <hghazni@gmail.com>
2019-07-13 20:18:09 +0100

    Adds aria-label for ESLint to emoji span

--- src/components/InstallationSuccess/InstallationSucess.tsx
+++ src/components/InstallationSuccess/InstallationSucess.tsx
@@ -5,9 +5,9 @@ export const InstallationSucess: React.SFC = () => {
     return (
         <div>
             <h1>
-                <span role={'image'}>🔥</span>
+                <span role={'img'} aria-label="Fire">🔥</span>
                 Congratulations, you now have a Killer Storybook Config
-                <span role={'image'}>🔥</span>
+                <span role={'img'} aria-label="Fire">🔥</span>
             </h1>
         </div>
     )
