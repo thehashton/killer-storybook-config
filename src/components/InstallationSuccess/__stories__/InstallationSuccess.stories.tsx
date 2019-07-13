73ef2430421e13a3ac77cb832900af39a91d74a6
Harry Ghazni <hghazni@gmail.com>
2019-07-13 20:13:11 +0100

    ESLint spacing

--- src/components/InstallationSuccess/__stories__/InstallationSuccess.stories.tsx
+++ src/components/InstallationSuccess/__stories__/InstallationSuccess.stories.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { withKnobs } from "@storybook/addon-knobs";
+
 import "../InstallationSuccess.scss";
 import { InstallationSucess } from "../InstallationSucess";
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.scss';
 import { InstallationSucess } from './components/InstallationSuccess/InstallationSucess';
 
